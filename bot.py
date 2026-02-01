5f7e867ed0d9bec314adc6c2b33a315ece34b36e
warawadekard-coder <warawadekard@gmail.com>
2026-02-01 12:32:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,7 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = 8508758212:AAH57JNhrIMScMfb0KM7gFeZf2wfzjievQE
-
+TOKEN = os.getenv(8508758212:AAH57JNhrIMScMfb0KM7gFeZf2wfzjievQE)
 # User score storage
 user_scores = {}
 
